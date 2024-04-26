66837175c5669028b1b5035f2062a84a40e9bd9c
Eduardo <144747945+eddchinea@users.noreply.github.com>
2024-04-26 15:36:45 -0400

    Add func to add each book in form of card to page

--- code.js
+++ code.js
@@ -24,3 +24,43 @@ addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
 
+
+// Display books in books container
+
+// Define books container
+const booksContainer = document.querySelector('.books-container');
+
+function addBookCardToPage () {
+    library.forEach(book => {
+        //Book card
+        const bookDiv = document.createElement('div');
+        const bookTitle = document.createElement('h4');
+        const bookAuthor = document.createElement('p');
+        const bookPages = document.createElement('p');
+        const readStatus = document.createElement('p');
+    
+        //Add classes to each book element
+        bookDiv.classList.add('book');
+        bookTitle.classList.add('book-title');
+        bookAuthor.classList.add('book-author');
+        bookPages.classList.add('book-pages');
+        readStatus.classList.add('book-read');
+    
+        //Add content for each book element
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages;
+        readStatus.textContent = book.read;
+    
+        // Append each book element to the book card
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(bookPages);
+        bookDiv.appendChild(readStatus);
+    
+        //Append the book card to the books container
+        booksContainer.appendChild(bookDiv);
+    })
+}
+
+addBookCardToPage();
